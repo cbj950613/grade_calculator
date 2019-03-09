12a3246026fcfcc91d53262b33cc2a4bc2f1afce
Tim Cho <timcho@Microsoft-XP.local>
2019-03-09 13:34:34 -0800

    started a new project

---
+++ grade_calculator.cpp
@@ -0,0 +1,29 @@
+#include "grade_calculator.h"
+#include "ui_grade_calculator.h"
+
+grade_calculator::grade_calculator(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::grade_calculator)
+{
+    ui->setupUi(this);
+    connect(ui->horizontalSlider,SIGNAL(valueChanged(int)),ui->hw1spinbox,SLOT(setValue(int)));
+    connect(ui->horizontalSlider_2,SIGNAL(valueChanged(int)),ui->midterm1spinbox,SLOT(setValue(int)));
+    QObject::connect(ui->hw1spinbox,SIGNAL(valueChanged(int)),
+                         this,SLOT(update_overall(int)));
+    QObject::connect(ui->midterm1spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall(int)));
+}
+
+grade_calculator::~grade_calculator()
+{
+    delete ui;
+}
+
+void grade_calculator::update_overall() {
+    double hw1 = ui->hw1spinbox->value();
+    double mt1 = ui->midterm1spinbox->value();
+
+    //double hw = hw1;
+    double overall = hw1+mt1;
+    ui->overall_grade->setText(QString::number(overall));
+}
---
+++ grade_calculator.h
@@ -0,0 +1,24 @@
+#ifndef GRADE_CALCULATOR_H
+#define GRADE_CALCULATOR_H
+
+#include <QMainWindow>
+#include <QObject>
+
+namespace Ui {
+class grade_calculator;
+}
+
+class grade_calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit grade_calculator(QWidget *parent = nullptr);
+    ~grade_calculator();
+private slots:
+    void update_overall();
+private:
+    Ui::grade_calculator *ui;
+};
+
+#endif // GRADE_CALCULATOR_H
