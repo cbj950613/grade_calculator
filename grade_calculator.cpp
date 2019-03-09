66565bf51214b92b28246650197f1abdc2f578c7
Tim Cho <timcho@Microsoft-XP.local>
2019-03-09 15:57:22 -0800

    first grading scheme works

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -6,11 +6,28 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui(new Ui::grade_calculator)
 {
     ui->setupUi(this);
-    connect(ui->horizontalSlider,SIGNAL(valueChanged(int)),ui->hw1spinbox,SLOT(setValue(int)));
-    connect(ui->horizontalSlider_2,SIGNAL(valueChanged(int)),ui->midterm1spinbox,SLOT(setValue(int)));
+
     QObject::connect(ui->hw1spinbox,SIGNAL(valueChanged(int)),
                          this,SLOT(update_overall()));
-    QObject::connect(ui->midterm1spinbox,SIGNAL(valueChanged(int)),
+    QObject::connect(ui->hw2spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw3spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw4spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw5spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw6spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw7spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw8spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->mt1spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->mt2spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->finalspinbox,SIGNAL(valueChanged(int)),
                      this,SLOT(update_overall()));
 }
 
@@ -21,9 +38,21 @@ grade_calculator::~grade_calculator()
 
 void grade_calculator::update_overall() {
     double hw1 = ui->hw1spinbox->value();
-    double mt1 = ui->midterm1spinbox->value();
+    double hw2 = ui->hw2spinbox->value();
+    double hw3 = ui->hw3spinbox->value();
+    double hw4 = ui->hw4spinbox->value();
+    double hw5 = ui->hw5spinbox->value();
+    double hw6 = ui->hw6spinbox->value();
+    double hw7 = ui->hw7spinbox->value();
+    double hw8 = ui->hw8spinbox->value();
+    double mt1 = ui->mt1spinbox->value();
+    double mt2 = ui->mt2spinbox->value();
+    double f = ui->finalspinbox->value();
 
-    //double hw = hw1;
-    double overall = hw1+mt1;
+    double hw_total = hw1 + hw2 + hw3 + hw4 + hw5 + hw6 + hw7 + hw8;
+    double overall = 0;
+
+    overall = hw_total/32+mt1/5+mt2/5+f*.35;
+
     ui->overall_grade->setText(QString::number(overall));
 }
