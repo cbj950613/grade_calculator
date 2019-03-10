fce180d98b3e12e873115be0324ac22b3131c997
Tim Cho <timcho@Microsoft-XP.local>
2019-03-09 16:07:35 -0800

    added two different grading schema

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -29,6 +29,41 @@ grade_calculator::grade_calculator(QWidget *parent) :
                      this,SLOT(update_overall()));
     QObject::connect(ui->finalspinbox,SIGNAL(valueChanged(int)),
                      this,SLOT(update_overall()));
+    QObject::connect(ui->schemaa,SIGNAL(clicked()),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->schemab,SIGNAL(clicked()),
+                     this,SLOT(update_overall()));
+
+    ui->schemaa->setChecked(1);
+
+    QObject::connect(ui->hw1spinbox,SIGNAL(valueChanged(int)),
+                         this,SLOT(update_overall()));
+    QObject::connect(ui->hw2spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw3spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw4spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw5spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw6spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw7spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->hw8spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->mt1spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->mt2spinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->finalspinbox,SIGNAL(valueChanged(int)),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->schemaa,SIGNAL(clicked()),
+                     this,SLOT(update_overall()));
+    QObject::connect(ui->schemab,SIGNAL(clicked()),
+                     this,SLOT(update_overall()));
+
+    ui->schemab->setChecked(1);
 }
 
 grade_calculator::~grade_calculator()
@@ -52,7 +87,11 @@ void grade_calculator::update_overall() {
     double hw_total = hw1 + hw2 + hw3 + hw4 + hw5 + hw6 + hw7 + hw8;
     double overall = 0;
 
-    overall = hw_total/32+mt1/5+mt2/5+f*.35;
-
+    if(ui->schemaa->isChecked())
+        overall = hw_total/32+mt1/5+mt2/5+f*.35;
+    if(ui->schemab->isChecked()){
+        double exam = ((mt1>mt2)?mt1:mt2);
+        overall = hw_total/32+exam*.3+f*.44;
+    }
     ui->overall_grade->setText(QString::number(overall));
 }
